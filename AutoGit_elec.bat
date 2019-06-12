68a6c70e603ac49f7ca61d77b0e33f76a26afc39
saafo <huang_haoting@126.com>
2019-06-12 12:56:33 +0800

    %date:~0,4%/%date:~5,2%/%date:~8,2% Update

--- AutoGit_elec.bat
+++ AutoGit_elec.bat
@@ -1,9 +1,7 @@
 @title 自动更新电费
 chcp 65001
-cd F:\Coding\Git_Space\electricityfee_display
-f:
 start python elec.py
-timeout /t 8
+timeout /t 5
 git add .
-git commit -m %date:~5,2%/%date:~8,2%AutoUpdate
+git commit -m %date:~0,4%/%date:~5,2%/%date:~8,2%AutoUpdate
 git push origin master
