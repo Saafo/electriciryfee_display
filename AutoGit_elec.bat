f88cd7f6a23eb8c69b9d2773448cefa9fec5669d
saafo <huang_haoting@126.com>
2019-06-12 13:24:28 +0800

    06/12 Auto Update

--- AutoGit_elec.bat
+++ AutoGit_elec.bat
@@ -3,5 +3,5 @@ chcp 65001
 start python elec.py
 timeout /t 5
 git add .
-git commit -m %date:~0,4%/%date:~5,2%/%date:~8,2%AutoUpdate
+git commit -m %date:~8,2%/%date:~11,2%" Auto Update"
 git push origin master
